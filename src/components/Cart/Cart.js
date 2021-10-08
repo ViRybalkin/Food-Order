e5f4cd92054380d7d40ce5570634065e34c587af
Рыбалкин Виталий Михайлович <vmrybalkin@phoenixit.ru>
2021-10-08 10:59:51 +0300

    Add functionality of decrementing and removing items in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,6 +6,7 @@ import classes from './Cart.module.scss';
 
 // = Components =
 import Modal from '../UI/Modal/Modal';
+import CartItem from './CartItem/CartItem';
 
 // = Context =
 import CartContext from '../../store/CartContext';
@@ -16,10 +17,24 @@ const Cart = props => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItem = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = id => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = item => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map(item => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.amount}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
